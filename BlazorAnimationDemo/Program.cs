1f2e1da6a6dcc3eb0c85aa217b6d2af832c1b2a5
Chris Neville - XPS17 <cneville@surgeforward.com>
2021-05-06 11:41:10 -0600

    Add project files.

---
+++ BlazorAnimationDemo/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorAnimationDemo
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            await builder.Build().RunAsync();
+        }
+    }
+}
